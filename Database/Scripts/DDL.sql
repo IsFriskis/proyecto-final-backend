e052659a2eec6cdcb994ba067aa40b732d95d20a
Marcos Martínez <marcos@mfgrupo.com>
2023-03-03 20:09:26 +0100

    Database Done with uuid

--- Database/Scripts/DDL.sql
+++ Database/Scripts/DDL.sql
@@ -5,32 +5,58 @@ create database if not exists db_proyectofinal;
 use db_proyectofinal;
 
 create table if not exists t_user(
-a_user_id integer primary key auto_increment,
+a_user_id varchar(36) primary key default (UUID()),
 a_user_name varchar(15) unique,
 a_user_mail varchar(25) unique,
 a_user_password varchar (50),
 a_user_isBan bool default false
 );
 
+create table if not exists t_bans(
+a_ban_id integer,
+a_ban_description varchar(200),
+a_ban_timestart timestamp default now(),
+a_ban_timeend timestamp, 
+a_ban_review boolean default false,
+a_user_id varchar(36),
+foreign key (a_user_id) references t_user(a_user_id)
+);
+
+create table if not exists t_rol(
+a_rol_id integer primary key auto_increment,
+a_rol_name varchar(20),
+a_rol_description varchar(100)
+);
+
 create table if not exists t_permissions(
 a_permission_id integer primary key auto_increment,
-a_permission_role enum("Standard", "Moderador", "Administrador"),
-a_user_id integer,
-foreign key (a_user_id) references t_user(a_user_id)
+a_rol_id integer not null,
+foreign key (a_rol_id) references t_rol(a_rol_id),
+a_user_id varchar(36),
+foreign key (a_user_id) references t_user(a_user_id) 
+);
+
+create table if not exists t_warnlevel(
+a_warnlevel_id integer primary key auto_increment,
+a_warnlevel_name varchar(20),
+a_warnlevel_description varchar(100)
 );
 
 create table if not exists t_warns(
 a_warn_id integer primary key auto_increment,
 a_warn_title varchar(30),
 a_warn_description varchar(200),
-a_warn_type enum("Leve", "Medio", "Grave"),
-a_user_id integer,
+a_warnlevel_id integer,
+foreign key (a_warnlevel_id) references t_warnlevel(a_warnlevel_id),
+a_warn_date timestamp default now(),
+a_user_id varchar(36),
 foreign key (a_user_id) references t_user(a_user_id)
 );
 
 create table if not exists t_match(
 a_match_id integer primary key auto_increment,
 a_match_score float,
-a_user_id integer,
+a_match_date timestamp,
+a_user_id varchar(36),
 foreign key (a_user_id) references t_user(a_user_id)
 );
--- Database/Scripts/DML.sql
+++ Database/Scripts/DML.sql
@@ -1,32 +1,61 @@
 use db_proyectofinal;
 
 /*Tabla de usuarios*/
-insert into t_user (a_user_name, a_user_mail, a_user_password, a_user_isBan)
-values("Usuario1", "Usuario1@gmail.com", "Abc123..", false);
+insert into t_user (a_user_name, a_user_mail, a_user_password)
+values("Usuario1", "Usuario1@gmail.com", "Abc123..");
 
-insert into t_user (a_user_name, a_user_mail, a_user_password, a_user_isBan)
-values("Usuario2", "Usuario2@gmail.com", "Abc123..", false);
+insert into t_user (a_user_name, a_user_mail, a_user_password)
+values("Usuario2", "Usuario2@gmail.com", "Abc123..");
 
-insert into t_user (a_user_name, a_user_mail, a_user_password, a_user_isBan)
-values("Usuario3", "Usuario3@gmail.com", "Abc123..", true);
+insert into t_user (a_user_name, a_user_mail, a_user_password)
+values("Usuario3", "Usuario3@gmail.com", "Abc123..");
+
+
+
+/*Tabla de roles*/
+insert into t_rol(a_rol_name, a_rol_description)
+values("Standard", "Es un rol para el 98% de los usuarios");
+
+insert into t_rol(a_rol_name, a_rol_description)
+values("Moderador", "Puede colocar warns a todos los usuarios");
+
+insert into t_rol(a_rol_name, a_rol_description)
+values("Administrador", "Es capaz de revisar las peticiones de ban");
+
+/*Tabla de niveles de warn*/
+insert into t_warnlevel(a_warnlevel_name, a_warnlevel_description)
+values("Leve", "Falta leve");
+
+insert into t_warnlevel(a_warnlevel_name, a_warnlevel_description)
+values("Media", "Falta de gravedad media");
+
+insert into t_warnlevel(a_warnlevel_name, a_warnlevel_description)
+values("Grave", "Falta grave");
 
 /*Tabla de Permissions*/
-insert into t_permissions(a_permission_role, a_user_id)
-values ("Administrador", 1);
+insert into t_permissions(a_rol_id, a_user_id)
+values (3, (SELECT a_user_id from t_user where a_user_name = "Usuario1"));
 
-insert into t_permissions(a_permission_role, a_user_id)
-values ("Moderador", 2);
+insert into t_permissions(a_rol_id, a_user_id)
+values (2, (SELECT a_user_id from t_user where a_user_name = "Usuario2"));
 
-insert into t_permissions(a_permission_role, a_user_id)
-values ("Standard", 3);
-
+insert into t_permissions(a_rol_id, a_user_id)
+values (1, (SELECT a_user_id from t_user where a_user_name = "Usuario3"));
 
 /*Tabla de Match*/
 insert into t_match(a_match_score, a_user_id)
-values (45000, 2);
+values (45000, (SELECT a_user_id from t_user where a_user_name = "Usuario2"));
 
 insert into t_match(a_match_score, a_user_id)
-values (35000, 3);
+values (35000, (SELECT a_user_id from t_user where a_user_name = "Usuario3"));
 
 insert into t_match(a_match_score, a_user_id)
-values (25000.54, 1);
+values (25000.54, (SELECT a_user_id from t_user where a_user_name = "Usuario1"));
+
+/*Tabla de bans*/
+insert into t_bans(a_ban_description, a_user_id)
+values("La lió el perro" , (SELECT a_user_id from t_user where a_user_name = "Usuario1"));
+
+/*Tabla de warns*/
+insert into t_warns(a_warn_title, a_warn_description, a_warnlevel_id, a_user_id)
+values("Griefing", "La lió parda", 3, (SELECT a_user_id from t_user where a_user_name = "Usuario1"));
